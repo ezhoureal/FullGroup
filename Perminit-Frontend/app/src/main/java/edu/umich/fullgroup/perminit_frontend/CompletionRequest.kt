bf8cdb8c36a6af9ec3ca82ed0360b44ee102a4d4
Brighton Pauli <brightp@umich.edu>
2022-03-11 15:54:05 -0500

    Created the CompletionRequest class
    This class sends https requests to openai to complete text prompts. Need to learn what "completion" function returns and how to access text.
    
    Co-Authored-By: bilalib <47580844+bilalib@users.noreply.github.com>

---
+++ Perminit-Frontend/app/src/main/java/edu/umich/fullgroup/perminit_frontend/CompletionRequest.kt
@@ -0,0 +1,30 @@
+package edu.umich.fullgroup.perminit_frontend
+
+import com.aallam.openai.api.completion.CompletionRequest
+import com.aallam.openai.api.engine.Davinci
+import com.aallam.openai.api.engine.Engine
+import com.aallam.openai.client.OpenAI
+import android.util.Log
+import kotlinx.coroutines.*
+
+object TextCompleter { 
+    val openAI = OpenAI("")
+    
+    fun onCreate() = runBlocking {
+        launch {
+            val davinci: Engine = openAI.engine(Davinci)
+        }
+    }
+
+    fun completeText(prompt: String) = runBlocking {
+        val completionRequest = CompletionRequest(prompt=prompt)
+        launch {
+            val completed = openAI.completion(Davinci, completionRequest)
+            return completed.choices.something // need to return the first choice given, for now
+        }
+    }
+}
+
+fun main() {
+    TextCompleter.completeText("Hello, world! My name is")
+}
