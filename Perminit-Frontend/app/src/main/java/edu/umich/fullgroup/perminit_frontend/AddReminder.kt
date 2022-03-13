5ace80ea03d972cd2dda535780e2dfcfa07537c1
Sam Wallace <samcw@umich.edu>
2022-03-13 10:59:42 -0400

    added a little view

---
+++ Perminit-Frontend/app/src/main/java/edu/umich/fullgroup/perminit_frontend/AddReminder.kt
@@ -0,0 +1,11 @@
+package edu.umich.fullgroup.perminit_frontend
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddReminder : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_reminder)
+    }
+}
--- Perminit-Frontend/app/src/main/java/edu/umich/fullgroup/perminit_frontend/MainActivity.kt
+++ Perminit-Frontend/app/src/main/java/edu/umich/fullgroup/perminit_frontend/MainActivity.kt
@@ -1,9 +1,12 @@
 package edu.umich.fullgroup.perminit_frontend
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+    fun addEvent(view: View?) = startActivity(Intent(this,AddReminder::class.java))
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
