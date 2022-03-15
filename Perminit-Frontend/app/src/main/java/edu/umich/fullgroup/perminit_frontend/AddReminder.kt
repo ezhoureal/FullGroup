ded292605e3f8acfaf9142f9edc715a18bbd278e
Tianer Zhou <zireael@umich.edu>
2022-03-15 13:26:40 -0400

    complete submit event

--- Perminit-Frontend/app/src/main/java/edu/umich/fullgroup/perminit_frontend/AddReminder.kt
+++ Perminit-Frontend/app/src/main/java/edu/umich/fullgroup/perminit_frontend/AddReminder.kt
@@ -3,42 +3,42 @@ package edu.umich.fullgroup.perminit_frontend
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import edu.umich.fullgroup.perminit_frontend.databinding.ActivityAddReminderBinding
+import java.lang.Exception
 import java.time.LocalDate
 import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 
 class AddReminder : AppCompatActivity() {
-    private lateinit var view: ActivityPostBinding
+    private lateinit var view: ActivityAddReminderBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_reminder)
-        view = ActivityPostBinding.inflate(layoutInflater)
+        view = ActivityAddReminderBinding.inflate(layoutInflater)
+        setContentView(view.root)
     }
 
     // called when submit button is pushed
     fun onSubmit() {
         // get data from view
-        val date = LocalDate.now()
-        val startTime = LocalTime.now()
-        val endTime = LocalTime.now()
-        val title = ""
+        val date: LocalDate = LocalDate.parse(view.editTextDate.text)
+        val startTime = LocalTime.parse(view.editTextStartTime.text)
+        val endTime = LocalTime.parse(view.editTextEndTime.text)
+        val title = view.NameField.text.toString()
         val perMinitId = 0
 
         // validation check
-        if (title.isEmpty()) {
+        if (title == "") {
             Toast.makeText(this, "Title is empty", Toast.LENGTH_SHORT).show()
-            return
-        } else if (startTime.) {
-            Toast.makeText(this, "Time not selected", Toast.LENGTH_SHORT).show()
             return
         }
 
         var event = Event(EventStore.id++, title, date, startTime, endTime, perMinitId)
-        if (notes) {
-            event.notes = notes
-        }
-        if (location) {
-            event.location = location
-        }
+//        if (view.notes) {
+//            event.notes = notes
+//        }
+//        if (location) {
+//            event.location = location
+//        }
         EventStore.add(event, date)
     }
 }
