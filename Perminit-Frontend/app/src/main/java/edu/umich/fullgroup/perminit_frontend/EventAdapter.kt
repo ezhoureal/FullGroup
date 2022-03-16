6b3c4dc19fe9546ebd406162bfbe12f100e77e96
Tianer Zhou <zireael@umich.edu>
2022-03-16 14:51:20 -0400

    to-do list event adapter

---
+++ Perminit-Frontend/app/src/main/java/edu/umich/fullgroup/perminit_frontend/EventAdapter.kt
@@ -0,0 +1,57 @@
+package edu.umich.fullgroup.perminit_frontend
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import java.time.LocalDate
+
+/**
+ * Adapter for the [RecyclerView] in [To-do List]. Displays [events] data object.
+ */
+class EventAdapter(
+    private val context: Context,
+    private val dataset: ArrayList<Event>
+) : RecyclerView.Adapter<EventAdapter.ItemViewHolder>() {
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder.
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+//        val textView: TextView = view.findViewById(R.id.item_title)
+    }
+
+    /**
+     * Create new views (invoked by the layout manager)
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        // create a new view
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent, false)
+
+        // manage events to display
+        for ((date, array) in EventStore.events) {
+            if (date >= LocalDate.now()) {
+                dataset += array
+            }
+        }
+        dataset.sortedWith(compareBy{ it.date })
+
+        return ItemViewHolder(adapterLayout)
+    }
+
+    /**
+     * Replace the contents of a view (invoked by the layout manager)
+     */
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = dataset[position]
+        holder.textView.text = context.resources.getString(item.stringResourceId)
+    }
+
+    /**
+     * Return the size of your dataset (invoked by the layout manager)
+     */
+    override fun getItemCount() = dataset.size
+}
