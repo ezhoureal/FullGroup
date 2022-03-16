757f99ba3223de7724d1a980a392f95efa53afb5
Tianer Zhou <zireael@umich.edu>
2022-03-16 15:14:01 -0400

    Update EventAdapter.kt

--- Perminit-Frontend/app/src/main/java/edu/umich/fullgroup/perminit_frontend/EventAdapter.kt
+++ Perminit-Frontend/app/src/main/java/edu/umich/fullgroup/perminit_frontend/EventAdapter.kt
@@ -1,6 +1,7 @@
 package edu.umich.fullgroup.perminit_frontend
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,19 +15,20 @@ import java.time.LocalDate
 class EventAdapter(
     private val context: Context,
     private val dataset: ArrayList<Event>
-) : RecyclerView.Adapter<EventAdapter.ItemViewHolder>() {
+) : RecyclerView.Adapter<EventAdapter.EventViewHolder>() {
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder.
-    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
-//        val textView: TextView = view.findViewById(R.id.item_title)
+    class EventViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val name: TextView = view.findViewById(R.id.eventName)
+        val time: TextView = view.findViewById(R.id.eventTime)
     }
 
     /**
      * Create new views (invoked by the layout manager)
      */
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventViewHolder {
         // create a new view
         val adapterLayout = LayoutInflater.from(parent.context)
             .inflate(R.layout.todo_item, parent, false)
@@ -39,15 +41,16 @@ class EventAdapter(
         }
         dataset.sortedWith(compareBy({ it.date }, { it.startTime }))
 
-        return ItemViewHolder(adapterLayout)
+        return EventViewHolder(adapterLayout)
     }
 
     /**
      * Replace the contents of a view (invoked by the layout manager)
      */
-    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: EventViewHolder, position: Int) {
         val item = dataset[position]
-//        holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.name.text = item.title
+        holder.time.text = item.startTime.toString()
     }
 
     /**
