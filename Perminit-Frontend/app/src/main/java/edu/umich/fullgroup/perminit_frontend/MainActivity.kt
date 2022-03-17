241de30a4845dcc02fa2b2445ac49bb7d82c2970
patrick9958 <patrick.neggie@gmail.com>
2022-03-17 15:38:11 -0400

    added notification channel

--- Perminit-Frontend/app/src/main/java/edu/umich/fullgroup/perminit_frontend/MainActivity.kt
+++ Perminit-Frontend/app/src/main/java/edu/umich/fullgroup/perminit_frontend/MainActivity.kt
@@ -1,7 +1,10 @@
 package edu.umich.fullgroup.perminit_frontend
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -30,6 +33,11 @@ class MainActivity : AppCompatActivity() {
         // Use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
         recyclerView.setHasFixedSize(true)
+
+        createNotificationChannel(
+            getString(R.string.event_notification_channel_id),
+            getString(R.string.event_notification_channel_name)
+        )
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -46,5 +54,28 @@ class MainActivity : AppCompatActivity() {
         EventStore.store(applicationContext)
         PerMinitStore.store(applicationContext)
     }
+
+    private fun createNotificationChannel(channelId: String, channelName: String) {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = channelName
+            val descriptionText = getString(R.string.event_notification_channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelId, name, importance).apply {
+                description = descriptionText
+            }
+
+//            TODO: maybe code below is better
+//            val notificationManager: NotificationManager = getSystemService(
+//                NotificationManager::class.java
+//            )
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
 
 }
