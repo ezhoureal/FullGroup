6dc8f4695f8d8dbabf467040ef110b4a6b4ef19f
Sam Wallace <samcw@umich.edu>
2022-03-18 00:04:24 -0400

    added chracters added minit seelction functionality made layout a little nicer

--- Perminit-Frontend/app/src/main/java/edu/umich/fullgroup/perminit_frontend/AddReminder.kt
+++ Perminit-Frontend/app/src/main/java/edu/umich/fullgroup/perminit_frontend/AddReminder.kt
@@ -3,9 +3,11 @@ package edu.umich.fullgroup.perminit_frontend
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import edu.umich.fullgroup.perminit_frontend.databinding.ActivityAddReminderBinding
+import java.io.Console
 import java.time.LocalDate
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
@@ -19,6 +21,33 @@ class AddReminder : AppCompatActivity() {
         setContentView(view.root)
     }
 
+    //maybe should be a default
+    var minit_selected = 0
+
+    fun select_1(viewk: View){
+
+        minit_selected=1
+
+
+        view.chooseText.setText("Selected " + view.pm1Name.text)
+
+        //R.id.
+
+
+        println(minit_selected)
+
+    }
+    fun select_2(viewk :View){
+        minit_selected=2
+        view.chooseText.setText("Selected " + view.pm2Name.text)
+
+    }
+    fun select_3(viewk:View){
+        view.chooseText.setText("Selected " + view.pm3Name.text)
+        minit_selected=3
+
+    }
+
     // called when submit button is pushed
     fun onSubmit(v: View) {
         // get data from view
@@ -27,7 +56,9 @@ class AddReminder : AppCompatActivity() {
         val startTime = LocalTime.parse(view.editTextStartTime.text)
         val endTime = LocalTime.parse(view.editTextEndTime.text)
         val title = view.NameField.text.toString()
-        val perMinitId = 0
+        val perMinitId = minit_selected
+
+        val description = view.descriptionbox.text.toString()
 
         // validation check
         if (title == "") {
@@ -47,6 +78,7 @@ class AddReminder : AppCompatActivity() {
         setResult(RESULT_OK, returnIntent);
         finish()
     }
+
 
     fun cancel(v: View) {
         finish()
