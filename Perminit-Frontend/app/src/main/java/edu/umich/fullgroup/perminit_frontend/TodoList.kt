b9ac4c3a17e3ce0445ce0a403dc9613c4b9e2d69
Tianer Zhou <zireael@umich.edu>
2022-03-21 10:23:52 -0400

    add edit event activity

--- Perminit-Frontend/app/src/main/java/edu/umich/fullgroup/perminit_frontend/TodoList.kt
+++ Perminit-Frontend/app/src/main/java/edu/umich/fullgroup/perminit_frontend/TodoList.kt
@@ -1,27 +1,28 @@
 package edu.umich.fullgroup.perminit_frontend
+// not in use
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
-
-class TodoList : AppCompatActivity() {
-    fun addEvent(view: View?) = startActivity(Intent(this,AddReminder::class.java))
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_todo_list)
-
-        val recyclerView = findViewById<RecyclerView>(R.id.RecyclerView)
-        recyclerView.adapter = EventAdapter(this, ArrayList())
-
-        // Use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
-        recyclerView.setHasFixedSize(true)
-
-        EventStore.load(applicationContext)
-        PerMinitStore.load(applicationContext)
-    }
-
-
-}
+//import android.content.Intent
+//import androidx.appcompat.app.AppCompatActivity
+//import android.os.Bundle
+//import android.view.View
+//import androidx.recyclerview.widget.RecyclerView
+//
+//class TodoList : AppCompatActivity() {
+//    fun addEvent(view: View?) = startActivity(Intent(this,AddReminder::class.java))
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_todo_list)
+//
+//        val recyclerView = findViewById<RecyclerView>(R.id.RecyclerView)
+//        recyclerView.adapter = EventAdapter(this, ArrayList())
+//
+//        // Use this setting to improve performance if you know that changes
+//        // in content do not change the layout size of the RecyclerView
+//        recyclerView.setHasFixedSize(true)
+//
+//        EventStore.load(applicationContext)
+//        PerMinitStore.load(applicationContext)
+//    }
+//
+//
+//}
