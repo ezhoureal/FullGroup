c8821be264f5dc4a2a22c34b5e12c3065f239e2d
Tianer Zhou <zireael@umich.edu>
2022-03-29 10:54:06 -0400

    Update CalendarActivity.kt

--- Perminit-Frontend/app/src/main/java/edu/umich/fullgroup/perminit_frontend/CalendarActivity.kt
+++ Perminit-Frontend/app/src/main/java/edu/umich/fullgroup/perminit_frontend/CalendarActivity.kt
@@ -2,10 +2,19 @@ package edu.umich.fullgroup.perminit_frontend
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CalendarView
+import java.time.LocalDate
 
 class CalendarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar)
+
+        var calendarView = findViewById<CalendarView>(R.id.calendarView)
+        calendarView.setOnDateChangeListener { calendarView, i, i2, i3 ->
+            var date: LocalDate = LocalDate.parse("$i3-$i2-$i")
+
+        }
     }
+
 }
