083705cb30f5cb7a88fc18cb96f207f49458beff
Tianer Zhou <zireael@umich.edu>
2022-03-29 10:32:48 -0400

    create calendar activity

---
+++ Perminit-Frontend/app/src/main/java/edu/umich/fullgroup/perminit_frontend/CalendarActivity.kt
@@ -0,0 +1,11 @@
+package edu.umich.fullgroup.perminit_frontend
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CalendarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calendar)
+    }
+}
