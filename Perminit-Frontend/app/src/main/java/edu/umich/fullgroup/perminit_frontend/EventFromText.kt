04223546ae6b8cb1b4564ecdbc67abaf32a372cb
Sam Wallace <samcw@umich.edu>
2022-04-03 10:15:33 -0400

    a bit of the template for arrays

--- Perminit-Frontend/app/src/main/java/edu/umich/fullgroup/perminit_frontend/EventFromText.kt
+++ Perminit-Frontend/app/src/main/java/edu/umich/fullgroup/perminit_frontend/EventFromText.kt
@@ -1,11 +1,27 @@
 package edu.umich.fullgroup.perminit_frontend
 
-import io.ktor.util.*
 import java.time.LocalDate
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 
 val RE = "((?<=event ).*) from (.*(?= on)) on (.*(?= to)) to (.*)$".toRegex()
+
+
+
+//add event play golf from 12:00 until 13:00 on 10/21/2022 with note bring my nine iron
+val eventNameKeywords = arrayOf("add event", "new event")
+val startTimeKeywords = arrayOf("from")
+val endTimeKeywords = arrayOf("until")
+val dateKeywords = arrayOf ("on")
+val descriptionKeyword = arrayOf ("with note")
+
+val nameRegexPart = eventNameKeywords.joinToString ( "|")
+
+
+
+
+
+var i = 0
 
 /*
     format MUST be:
@@ -14,6 +30,7 @@ val RE = "((?<=event ).*) from (.*(?= on)) on (.*(?= to)) to (.*)$".toRegex()
     MMM = month abbreviation (Jan, Feb, ...). Cannot write full month name
  */
 fun makeEvent (input : String): Event? {
+    println (nameRegexPart)
     val parsed = RE.find(input.lowercase())
     // groupValues: 1- event name, 2- start time, 3- start date, 4- end time
     val name = parsed?.groupValues?.get(1)
@@ -21,8 +38,10 @@ fun makeEvent (input : String): Event? {
     val dateStr = parsed?.groupValues?.get(3) + " " + LocalDate.now().year
     val endTimeStr = parsed?.groupValues?.get(4)?.uppercase()
 
-    val dateFormatter = DateTimeFormatter.ofPattern("MMM d yyyy")
+    val dateFormatter = DateTimeFormatter.ofPattern("MM dd yyyy")
+    //well it doesn't sem to like this
     val date = LocalDate.parse(dateStr, dateFormatter)
+
 
     val timeFormatter = DateTimeFormatter.ofPattern("h[:mm] a")
     val startTime = LocalTime.parse(startTimeStr, timeFormatter)
